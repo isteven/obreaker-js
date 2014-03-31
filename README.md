9ca3f0dbaf2e314e4baf97214b0f70340a99f647
isteven <isteven@server.fake>
2014-03-31 14:49:41 +0800

    first entry

---
+++ README.md
@@ -0,0 +1,38 @@
+Obreaker Js Object Debugger
+===========================
+A script which traverse and print object properties recursively to your web browser console.
+
+
+<br />![Screenshot](https://raw.githubusercontent.com/isteven/obreaker/master/screenshot.jpg)
+
+Demo: http://jsfiddle.net/T69b3/1/ or download all the files into a same folder and open obreaker.htm.
+
+Features
+--
+  - Print everything. Expression, string, object, array.. all in one liner!
+  - Different closures, [] for array and {} for object so you can easily differentiate them
+  - Set how deep you would like to traverse the object. Default is 999.
+  
+Usage
+--
+Load obreaker.js and use it like this:
+
+    obreaker.log( 'hello world!', 'my host is: ' , window.location.host );
+
+(Similar to console.log)
+
+Note
+--
+On Firebug's console, it displays function name & line number of the caler properly. But it simply display the obreaker.js on Chrome's and/or IE's console.
+
+Browser Compatibility
+--
+Tested on:
+- Opera 12.16 (Yes, I love Opera browser! Download yours here: http://www.opera.com/)
+- IE8 (IE8 is the default browser in the company I work for)
+- Firefox 27
+- Google Chrome 33
+
+Licence
+--
+Released under the MIT license. 
---
+++ obreaker.js
@@ -0,0 +1,211 @@
+/* 
+ * Obreaker
+ * A debuger to traverse and print object properties recursively to your web
+ * browser console.
+ *
+ * Released under MIT License
+ * --------------------------------------------------------------------------------
+ * MIT License
+ *
+ * Copyright (C) 2014 Ignatius Steven
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in 
+ * the Software without restriction, including without limitation the rights to 
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+ * the Software, and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions: 
+ *
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
+ * --------------------------------------------------------------------------------
+ */
+
+obreaker = 
+{     
+    /////////////
+    // SETTINGS
+    /////////////
+
+    // Debug message prefix
+    console_prefix: '[obreaker]',
+    
+    // Traverse depth: 
+    traverse_depth : 999,
+    
+    /*
+     * Debug to console
+     *
+     * Usage sample:
+     *
+     * name = 'Bill';
+     * data = 
+     * {
+     *      { 
+     *          name: 'John' 
+     *      },
+     *      { 
+     *          name: 'Jane' 
+     *      }
+     * };
+     * obreaker.log( 'a string', name, data, 'another string' [, ...] );
+     */
+
+    log: function() 
+    {               
+
+        var toPrint = '';
+        
+
+        for ( var key in arguments )
+        {
+            if ( typeof arguments[ key ] !== 'object' )
+            {
+                if ( key == 0 )
+                {
+                    sep = '';
+                }
+                else
+                {
+                    if ( toPrint != '') 
+                    {
+                        sep = ' > ';
+                    }
+                }
+                toPrint = toPrint + sep + arguments[ key ];
+            }
+            else 
+            {
+                if ( toPrint != '' )
+                {
+                    var errorStack = new Error();
+                    var errorInfo = '';
+                    if ( typeof errorStack.stack != 'undefined' )
+                    {
+                        errorStack = errorStack.stack.split("\n")[1];                        
+                        errorInfo = ' (' + this.parseError( errorStack ) + ')';
+                    }
+                    console.log( this.console_prefix + ' ' + '   ' + toPrint + errorInfo );
+                }
+
+                toPrint = '';                
+                var closure = this.getClosure( arguments[ key ] );
+                console.log( this.console_prefix + '    ' + closure[ 'open' ] );
+                this.print_object( arguments[ key ] , '   ' )
+                console.log( this.console_prefix + '    ' + closure[ 'close' ] );
+            }
+        }
+        if ( toPrint != '' )
+        {
+            var errorStack = new Error();
+            var errorInfo = '';
+            if ( typeof errorStack.stack != 'undefined' )
+            {
+                errorStack = errorStack.stack.split("\n")[1];                        
+                errorInfo = ' (' + this.parseError( errorStack ) + ')';
+            }
+            console.log( this.console_prefix + ' ' + '   ' + toPrint + errorInfo );
+        }
+    },
+
+    /*
+     * Recursive method to print object
+     * Reference: http://stackoverflow.com/questions/15058780/eclipse-ide-console-log-objects-in-javascript
+     */    
+    print_object: function( obj, padding, currLevel )
+    {
+        var ind = '';
+
+        if ( currLevel == '' || typeof currLevel === 'undefined' || currLevel == null ) 
+        {
+            currLevel = 0;
+        }
+
+        if ( currLevel == this.traverse_depth ) 
+        {
+            return true;
+        }
+
+        if (arguments.length > 1)
+        {
+            ind = arguments[ 1 ];
+        }
+
+        if ( typeof obj === 'undefined' || obj == null )
+        {
+            console.log( '<null>' );
+            return;
+        }
+
+        for (var key in obj)
+        {
+            if ( typeof obj[key] === 'function' ) 
+            {
+                continue;
+            }            
+            var closure = this.getClosure( obj[key] );
+            if ( typeof obj[key] === 'object' && obj.hasOwnProperty( key ) )
+            {
+                console.log( this.console_prefix + '   ' + ind + key + ': ' );
+                console.log( this.console_prefix + '   ' + ind + closure[ 'open' ] );
+                this.print_object( obj[ key ], ind + '   ' , currLevel + 1 );
+                console.log( this.console_prefix + '   ' + ind + closure[ 'close' ] );
+            }
+            else
+            {
+                console.log( this.console_prefix + '   ' + ind + key + ': ' + obj[ key ] );
+            }
+        }
+    },
+
+    getClosure: function( item ) {
+        var closure     = new Array();
+        var objectType  = typeof item;
+
+        if ( objectType === 'object' ) {
+            if ( item ) {
+                if ( item instanceof Array) {
+                    objectType = 'array';
+                }
+            } else {
+                objectType = 'null';
+            }
+        }
+        
+        switch( objectType ) {
+            case 'array':
+                closure[ 'open' ] = '[';
+                closure[ 'close' ] = ']';
+                break;
+            case 'object':
+                closure[ 'open' ] = '{';
+                closure[ 'close' ] = '}';
+                break;                    
+            default:                        
+                closure[ 'open' ] = '{';
+                closure[ 'close' ] = '}';
+                break;
+        }
+        return closure;
+    },
+
+    parseError: function( error ) {
+        var errorArr = error.split( /[@|/]/ ),
+            functionName = '';
+        if ( errorArr[0] ) 
+        {
+            functionName = errorArr[0] + '() - ';
+        }
+        var scriptLine = errorArr[ errorArr.length - 1];            
+            scriptLine = scriptLine.split( ':' );
+        return ( functionName + scriptLine[0] + ' [line ' + scriptLine[1] + ']');
+    }
+}
